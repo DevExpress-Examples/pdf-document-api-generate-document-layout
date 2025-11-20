589a3c3240c8467d9a5fa171bef6b49bdba1af9f
Anthony Sam <anton.sameishchev@devexpress.com>
2025-11-20 15:22:19 +0100

    update

--- CS/DocumentCreationAPI/Program.cs
+++ CS/DocumentCreationAPI/Program.cs
@@ -15,7 +15,7 @@ namespace DocumentCreationAPI {
                 // Create an empty document.
                 processor.CreateEmptyDocument(docPath);
                 // Create and draw PDF graphics.
-                using (PdfGraphics graph = processor.CreateGraphics()) {
+                using (PdfGraphics graph = processor.CreateGraphicsWorldSystem()) {
                     DrawGraphics(graph);
                     // Render a page with graphics.
                     processor.RenderNewPage(PdfPaperSize.Letter, graph);
@@ -31,14 +31,14 @@ namespace DocumentCreationAPI {
             // Draw text lines on the page.
             DXSolidBrush black = (DXSolidBrush)DXBrushes.Black;
             DXFont font1 = new DXFont("Times New Roman", 32, DXFontStyle.Bold);
-            graph.DrawString("PDF Document Processor", font1, black, 180, 150);
+            graph.DrawString("PDF Document Processor", font1, black, 140, 50);
 
             DXFont font2 = new DXFont("Arial", 20);
-            graph.DrawString("Display, Print and Export PDF Documents", font2, black, 168, 230);
+            graph.DrawString("Display, Print and Export PDF Documents", font2, black, 128, 120);
 
             DXFont font3 = new DXFont("Arial", 10);
             graph.DrawString("The PDF Document Processor is a non-visual component " +
-                              "that provides the application programming interface of the PDF Viewer.", font3, black, 16, 300);
+                              "that provides the application programming interface of the PDF Viewer.", font3, black, 16, 180);
         }
 
         // Add a watermark with custom text.
--- VB/DocumentCreationAPI/Program.vb
+++ VB/DocumentCreationAPI/Program.vb
@@ -33,13 +33,13 @@ Namespace DocumentCreationAPI
             ' Draw text lines on the page.
             Dim black As DXSolidBrush = CType(DXBrushes.Black, DXSolidBrush)
             Dim font1 As DXFont = New DXFont("Times New Roman", 32, DXFontStyle.Bold)
-            graph.DrawString("PDF Document Processor", font1, black, 180, 150)
+            graph.DrawString("PDF Document Processor", font1, black, 140, 50)
 
             Dim font2 As DXFont = New DXFont("Arial", 20)
-            graph.DrawString("Display, Print and Export PDF Documents", font2, black, 168, 230)
+            graph.DrawString("Display, Print and Export PDF Documents", font2, black, 128, 120)
 
             Dim font3 As DXFont = New DXFont("Arial", 10)
-            graph.DrawString("The PDF Document Processor is a non-visual component " & "that provides the application programming interface of the PDF Viewer.", font3, black, 16, 300)
+            graph.DrawString("The PDF Document Processor is a non-visual component " & "that provides the application programming interface of the PDF Viewer.", font3, black, 16, 180 )
         End Sub
 
         ' Add a watermark with custom text.
